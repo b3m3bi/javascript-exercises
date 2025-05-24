458c4c3300351da13cbbfbda41bc7161fe9751a4
Luis Garcia <46090720+b3m3bi@users.noreply.github.com>
2025-05-23 19:21:54 -0600

    feat: add answers to js excercise 9 sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(number1, number2) {
+    if( (number1 < 0 || number2 < 0) || 
+        (number1 - Math.floor(number1) > 0 || number2 - Math.floor(number2) > 0) ||
+        (typeof number1 !== "number" || typeof number2 !== "number")
+    ) {
+        return "ERROR"
+    }
+    let geaterNum = number1 > number2 ? number1 : number2;
+    let smallerNum = number1 <= number2 ? number1 : number2;
+    let sum = 0;
+    for(let i = smallerNum; i <= geaterNum; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
