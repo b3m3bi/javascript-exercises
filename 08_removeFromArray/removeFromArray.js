c5699058557760982b004b1f9c8c57d8ce9e25c7
Luis Garcia <46090720+b3m3bi@users.noreply.github.com>
2025-05-23 19:02:28 -0600

    feat: add answers to js excercise 8 removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removeVals) {
+    for(let removeVal of removeVals){
+      while(arr.includes(removeVal)){
+        let indexVal = arr.findIndex(elem => elem === removeVal);
+        arr = arr.slice(0, indexVal).concat(arr.slice(indexVal + 1));
+      }
+    }
+    return arr;
 };
 
 // Do not edit below this line
