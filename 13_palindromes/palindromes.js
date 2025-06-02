3738b75002074eccecaaa1026fabcd8602af55e5
Luis Garcia <46090720+b3m3bi@users.noreply.github.com>
2025-06-02 09:18:57 -0600

    feat: add answers to js excercise 13 palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let wordLettersOnly = word.split('').filter(char => {
+        return /[a-zA-Z0-9]/.test(char)
+    }).join('').toLowerCase();
+    let reverse = wordLettersOnly.split('').reverse().join('');
+    return reverse === wordLettersOnly ? true : false;
 };
 
 // Do not edit below this line
