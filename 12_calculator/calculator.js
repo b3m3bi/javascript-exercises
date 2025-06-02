88d92c69209ceaea5d86de7dc4eaabb326eeeaa9
Luis Garcia <46090720+b3m3bi@users.noreply.github.com>
2025-06-02 08:59:30 -0600

    feat: add answers to js excercise 12 calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, val) => sum += val, 0); 
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((mul, val) => mul *= val, 1);
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	return num ** pow
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {
+    return 1
+  } else {
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
