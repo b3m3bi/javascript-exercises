c4b55552a3b9398ad2574b168ef7309d0a632e44
Luis Garcia <46090720+b3m3bi@users.noreply.github.com>
2025-06-02 09:36:21 -0600

    feat: add answers to js excercise 14 fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  let result;
+  if (+num < 0) {
+    result = 'OOPS';
+  } else if (+num === 0) {
+    result = 0;
+  } else if (+num === 1) {
+    result = 1;
+  } else {
+    series = [1, 1];
+    let i = 2;
+    while (i < +num) {
+      let nextNumber = series[i - 2] + series[i - 1];
+      series.push(nextNumber);
+      i++;
+    }
+    result = series[series.length -1]
+  }
 
+  return result;
 };
 
 // Do not edit below this line
