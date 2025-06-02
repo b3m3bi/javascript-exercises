70eb395d3b97692b4412d123be91cfc177b29642
Luis Garcia <46090720+b3m3bi@users.noreply.github.com>
2025-06-02 10:03:52 -0600

    feat: add answers to js excercise 16 find the oldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(peopleArr) {
+    let maxAge = peopleArr.map(p => {
+        if (p.yearOfDeath){
+            return p.yearOfDeath - p.yearOfBirth
+        } else {
+            return 2025 - p.yearOfBirth
+        }
+    }).sort((a, b) => b - a)[0]
+    return peopleArr.filter(p => {
+        if (p.yearOfDeath){
+            return p.yearOfDeath - p.yearOfBirth === maxAge
+        } else {
+            return 2025 - p.yearOfBirth === maxAge
+        }
+    }).pop()
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
